49077488cbfa19b2bbf847b9a02ae59322dd5ec0
leandro <santos.leandrom@outlook.com>
2022-02-15 22:25:09 -0300

    remove commented sprite class

--- source/core/graphics.h
+++ source/core/graphics.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "vector2.h"
+
 class Texture{
     private:
      void* bitmap = nullptr;
@@ -15,14 +16,6 @@ class Texture{
         }
 };
 
-//class Sprite{
-//    public:
-//        Texture *texture;
-//        int x = 0;
-//        int y = 0;
-//        int w = 32;
-//        int h = 32;
-//};
 
 struct Rectangle{
     int x;
@@ -38,7 +31,6 @@ class IGraphics{
         virtual void init(){}
         virtual void draw(const Vector2 &position, const Rectangle &rect, void* texture){}
         virtual void drawTexture(float x, float y, void* texture){}
-        //virtual void drawSprite(float x, float y, Sprite &sprite){}
         virtual void drawTile(float x, float y, float src_x, float src_y, int tilesize, void* texture){}
         virtual void drawRectangle(float x, float y, float w, float h){}
         virtual void drawText(const char* text, float x, float y){}
